4f58b2675ef146dc81cf9782a5fdc8ed3ac9fc69
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-09-12 13:35:49 +0500

    feat: integration GraphQLModule and build first GraphQL api

--- apps/nestar-api/src/app.service.ts
+++ apps/nestar-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to Nestar APi Server!';
+    return 'Welcome to Nestar REST API Server!';
   }
 }
