88669a6905feb7e77347d175d97743fc37091623
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-09-25 21:43:46 +0900

    feat: develop Logging via global interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,17 +1,39 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+	private readonly logger: Logger = new Logger();
 
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
-      );
-  }
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const recordTime = Date.now();
+		console.log('contect+:', context);
+		const requestType = context.getType<GqlContextType>();
+		console.log('requestType:+', requestType);
+
+		if (requestType === 'http') {
+			/* Develop if needed!  */
+		} else if (requestType === 'graphql') {
+			/* (1) Print Request */
+			const gqlContext = GqlExecutionContext.create(context);
+			console.log('gqlContext++', gqlContext);
+			this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+
+			/* (2) Error handling via GraphQL */
+
+			/* (3) No Errors, giving Response below */
+			return next.handle().pipe(
+				tap((contect) => {
+					const responseTime = Date.now() - recordTime;
+					this.logger.log(`${this.stringify(contect)} - ${responseTime}ms \n\n`, 'RESPONSE');
+				}),
+			);
+		}
+	}
+
+	private stringify(context: ExecutionContext): string {
+		return JSON.stringify(context).slice(0, 75);
+	}
 }
